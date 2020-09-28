d21a88ac5cfb569d9fd17cf240df648bed87ca30
threeteck <threeteck@gmail.com>
2020-09-28 19:07:46 +0300

    Changed realisation

--- CalculatorF/Program.fs
+++ CalculatorF/Program.fs
@@ -18,45 +18,24 @@ type MaybeBuilder() =
 let maybe = new MaybeBuilder()
 
 let add x y =
-    maybe
-        {
-        let! a = x
-        let! b = y
-        return a + b
-        }
+    x + y
         
 let subtract x y =
-    maybe
-        {
-        let! a = x
-        let! b = y
-        return a - b
-        }
+    x - y
         
 let divide x y =
-    maybe
-        {
-        let! a = x
-        let! b = 
-            match y with
-            | Some 0.0 | None -> None
-            | Some c -> Some c
-        return a / b
-        }
+    match y with
+    | 0.0 -> None
+    | _ -> Some(x/y)
 
 let multiply x y =
-    maybe
-        {
-        let! a = x
-        let! b = y
-        return a * b
-        }
+    x * y
         
 let calculate op x y =
             match op with
-            | "+" -> add x y
-            | "*" -> multiply x y
-            | "-" -> subtract x y
+            | "+" -> Some(add x y)
+            | "*" -> Some(multiply x y)
+            | "-" -> Some(subtract x y)
             | "/" -> divide x y
             | _ -> None
     
@@ -65,8 +44,13 @@ let main argv =
     let a = Console.ReadLine() |> Double.TryParse |> tryParseResultToOption
     let op = Console.ReadLine()
     let b = Console.ReadLine() |> Double.TryParse |> tryParseResultToOption
-    let result = calculate op a b
+    let result = maybe {
+            let! x = a
+            let! y = b
+            let! calc = calculate op x y
+            return calc
+        }
     match result with
     | None -> printf "Error occurred"
-    | Some a -> printf "Result: %f" a
+    | Some v -> printf "Result: %f" v
     0
